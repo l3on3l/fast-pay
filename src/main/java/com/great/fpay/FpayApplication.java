b1e25f2bb8915fbf35a71edcc835ef20db98c664
fvera <fvera@sodep.com.py>
2024-10-01 21:49:03 -0400

    initial commit

---
+++ src/main/java/com/great/fpay/FpayApplication.java
@@ -0,0 +1,13 @@
+package com.great.fpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/great/fpay/FpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.great.fpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
